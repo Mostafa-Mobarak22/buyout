9bbb4c335b08ab6f36c65dad5eb93edd594b1c5c
MichaelAwadEissa <michaelawadeissa@gmail.com>
2024-09-30 18:32:56 +0300

    profile page

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,18 @@ import Profile from './componants/profile/Profile'
 import Layout from './componants/layout/Layout'
 import Navbar from './componants/navbar/Navbar'
 import Loading from './componants/loading/Loading'
-
+import ProfilePage from '../src/pages/Profile/Profile'
+import LandingPage from './pages/landin_page/landing_page'
 function App() {
   const [count, setCount] = useState(0)
 
   const route = createBrowserRouter([
     {path: "",element: <Layout/>,children:[
+      {path: "",element: <LandingPage/>},
       {path: "login",element: <Login/>},
-      {path: "signup",element: <Register/>}
+      {path: "signup",element: <Register/>},
+      {path: "profile",element: <ProfilePage/>}
+
     ]}
   ])
 
